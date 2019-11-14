afa3d49e6820f4da46530e4b6d3278b8de2b8f92
ysj <1584809881@qq.com>
2019-11-14 22:09:45 +0800

    优化用户接口,新增https

--- 后端/src/main/java/com/example/datajdbc/mapper/UserMapper.java
+++ 后端/src/main/java/com/example/datajdbc/mapper/UserMapper.java
@@ -3,14 +3,23 @@ package com.example.datajdbc.mapper;
 import com.example.datajdbc.bean.User;
 import org.apache.ibatis.annotations.*;
 
-@Mapper
+import java.util.List;
+
+//@Mapper
 public interface UserMapper {
-    @Select("select * from _User where id=#{id}")
+    @Select("select * from _User where userId=#{id}")
     public User getUserById(Integer id);
 
-    @Delete("delete * from _User where id =#{id}")
-    public int deleteUserById(Integer id);
+    @Select("select * from _User")
+    public List<User> getAllUser();
 
+    @Delete("delete from _User where userId =#{id}")
+    public void deleteUserById(Integer id);
+
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
     @Insert("insert into _User(userName,userPsd,userSex) values(#{userName},#{userPsd},#{userSex})")
-    public
+    public void insertUser(User user);
+
+    @Update("update _User set userName=#{userName} where userId=#{id}")
+    public void updateUser(User user);
 }
